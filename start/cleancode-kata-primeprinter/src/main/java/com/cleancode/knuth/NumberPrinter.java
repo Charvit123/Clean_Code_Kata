5ea34c0ffd67a1dacc1fae55f5d7c7e5ac808336
cardeshna <cardeshna@republicfinance.com>
2024-04-30 15:26:16 +0530

    Some Refactoring in NumberPrinter

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
@@ -1,26 +1,42 @@
 package com.cleancode.knuth;
 
 public class NumberPrinter {
+
+  public static final int COLS_PER_PAGE = 4;
+  public static final int ROWS_PER_PAGE = 50;
+
   public void printNumbers(int totalNumbers, int[] numberList) {
     int pagenumber = 1;
     int pageoffset = 1;
-    final int colsPerPage = 4;
-    final int rowsPerPage = 50;
     while (pageoffset <= totalNumbers) {
-      System.out.print("The First ");
-      System.out.print(Integer.toString(totalNumbers));
-      System.out.print(" Prime Numbers === Page ");
-      System.out.print(Integer.toString(pagenumber));
-      System.out.println("\n");
-      for (int rowoffset = pageoffset; rowoffset <= pageoffset + rowsPerPage - 1; rowoffset++) {
-        for (int columnNo = 0; columnNo <= colsPerPage - 1; columnNo++)
-          if (rowoffset + columnNo * rowsPerPage <= totalNumbers)
-            System.out.printf("%10d", numberList[rowoffset + columnNo * rowsPerPage]);
-        System.out.println();
-      }
+      printHeader(totalNumbers, pagenumber);
+      printPage(totalNumbers, numberList, pageoffset);
       System.out.println("\f");
       pagenumber++;
-      pageoffset += rowsPerPage * colsPerPage;
+      pageoffset += ROWS_PER_PAGE * COLS_PER_PAGE;
     }
+  }
+
+  private static void printPage(int totalNumbers, int[] numberList, int pageoffset) {
+    for (int rowoffset = pageoffset; rowoffset <= pageoffset + ROWS_PER_PAGE - 1; rowoffset++) {
+      printRow(totalNumbers, numberList, rowoffset);
+    }
+  }
+
+  private static void printRow(int totalNumbers, int[] numberList, int rowoffset) {
+    for (int columnNo = 0; columnNo <= COLS_PER_PAGE - 1; columnNo++) {
+      if (rowoffset + columnNo * ROWS_PER_PAGE <= totalNumbers) {
+        System.out.printf("%10d", numberList[rowoffset + columnNo * ROWS_PER_PAGE]);
+      }
+    }
+    System.out.println();
+  }
+
+  private static void printHeader(int totalNumbers, int pagenumber) {
+    System.out.print("The First ");
+    System.out.print(Integer.toString(totalNumbers));
+    System.out.print(" Prime Numbers === Page ");
+    System.out.print(Integer.toString(pagenumber));
+    System.out.println("\n");
   }
 }
