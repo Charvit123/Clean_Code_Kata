caf25be22e1efdbae6745f08faa23a51ceb97c6d
cardeshna <cardeshna@republicfinance.com>
2024-04-30 13:30:33 +0530

    does some refactoring

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimeGenerator.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimeGenerator.java
@@ -1,40 +1,50 @@
 package com.cleancode.knuth;
 
 public class PrimeGenerator {
-  public int[] generate() {
 
-    final int numberOfPrimes = 1000;
-    int[] primes = new int[numberOfPrimes + 1];
+  final int maxPrimeOrder = 30;
+  boolean isPrime;
+  int[] primes;
+  int[] primeMultiples = new int[maxPrimeOrder + 1];
+  int primeOrder;
+  int primeIndex;
+  int number;
+  int currentOddNumber;
+  int square;
+
+  public int[] generate(int numberOfPrimes) {
+    number = 0;
+    currentOddNumber = 1;
+    primeIndex = 1;
+    primeOrder = 2;
+    primes = new int[numberOfPrimes + 1];
     primes[1] = 2;
-
-    int number = 0;
-    final int maxPrimeOrder = 30;
-    int[] primeMultiples = new int[maxPrimeOrder + 1];
-    int currentOddNumber = 1;
-    int primeIndex = 1;
-    int primeOrder = 2;
-    int square = 9;
+    square = 9;
     while (primeIndex < numberOfPrimes) {
-      boolean isPrime;
-      do {
-        currentOddNumber += 2;
-        if (currentOddNumber == square) {
-          primeOrder++;
-          square = primes[primeOrder] * primes[primeOrder];
-          primeMultiples[primeOrder - 1] = currentOddNumber;
-        }
-        number = 2;
-        isPrime = true;
-        while (number < primeOrder && isPrime) {
-          while (primeMultiples[number] < currentOddNumber)
-            primeMultiples[number] += primes[number] + primes[number];
-          if (primeMultiples[number] == currentOddNumber) isPrime = false;
-          number++;
-        }
-      } while (!isPrime);
+      square = findNextPrime();
       primeIndex++;
       primes[primeIndex] = currentOddNumber;
     }
     return primes;
+  }
+
+  private int findNextPrime() {
+    do {
+      currentOddNumber += 2;
+      if (currentOddNumber == square) {
+        primeOrder++;
+        square = primes[primeOrder] * primes[primeOrder];
+        primeMultiples[primeOrder - 1] = currentOddNumber;
+      }
+      number = 2;
+      isPrime = true;
+      while (number < primeOrder && isPrime) {
+        while (primeMultiples[number] < currentOddNumber)
+          primeMultiples[number] += primes[number] + primes[number];
+        if (primeMultiples[number] == currentOddNumber) isPrime = false;
+        number++;
+      }
+    } while (!isPrime);
+    return square;
   }
 }
--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -2,9 +2,9 @@ package com.cleancode.knuth;
 
 public class PrimePrinter {
   public static void main(String[] args) {
-    PrimeGenerator primeGenerator = new PrimeGenerator();
-    int[] primes = primeGenerator.generate();
     int numberOfPrimes = 1000;
+    PrimeGenerator primeGenerator = new PrimeGenerator();
+    int[] primes = primeGenerator.generate(numberOfPrimes);
     NumberPrinter numberPrinter = new NumberPrinter();
     numberPrinter.printNumbers(numberOfPrimes, primes);
   }
